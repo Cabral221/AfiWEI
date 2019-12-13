22e069c28f669235ff1bcb9ebb416ff94b1903e8
Abdourahmane Diop <cabraldiop18@gmail.com>
2019-12-13 11:29:42 +0000

    db seed for one To 22 rooms

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,44 +14,37 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // Creation des Chambres
-        for ($i=23; $i < 43; $i++) { 
-            if($i == 32 || $i == 34){
-                Room::create([
-                    'libele' => 'chambre '.$i,
-                    'qntMax' => 6,
-                ]);
-            }else{
-                Room::create([
-                    'libele' => 'chambre '.$i,
-                    'qntMax' => 5,
-                ]);
-            }
-        }
-
-        //Creation des niveaux d'etudes Licence
-        for ($i=1; $i < 4; $i++) { 
-            Niveau::create([
-                'libele' => 'Licence '.$i,
-                'sigle' => 'L'.$i,
-            ]);
-        }
-        //Creation des niveaux d'etudes Master
-        for ($i=1; $i < 3; $i++) { 
-            Niveau::create([
-                'libele' => 'Master '.$i,
-                'sigle' => 'M'.$i,
+        // // Creation des Chambres
+        for ($i=1; $i < 23; $i++) { 
+            Room::create([
+                'libele' => 'chambre '.$i,
+                'qntMax' => 5,
             ]);
         }
 
-        //Creation des filiéres
-        Sector::create(['libele' => 'Banquue Finances Assurances','sigle' => 'BAF']);
-        Sector::create(['libele' => 'Gestion des resources humaines','sigle' => 'GRH']);
-        Sector::create(['libele' => 'Genie Logiciel','sigle' => 'GELO']);
-        Sector::create(['libele' => 'Informatiques et réseaux','sigle' => 'IR']);
-        Sector::create(['libele' => 'Management des affaires internationales','sigle' => 'MAI']);
-        Sector::create(['libele' => 'Marketing Management Communication','sigle' => 'MMC']);
-        Sector::create(['libele' => 'Systéme réseaux et telecom','sigle' => 'SRT']);
+        // //Creation des niveaux d'etudes Licence
+        // for ($i=1; $i < 4; $i++) { 
+        //     Niveau::create([
+        //         'libele' => 'Licence '.$i,
+        //         'sigle' => 'L'.$i,
+        //     ]);
+        // }
+        // //Creation des niveaux d'etudes Master
+        // for ($i=1; $i < 3; $i++) { 
+        //     Niveau::create([
+        //         'libele' => 'Master '.$i,
+        //         'sigle' => 'M'.$i,
+        //     ]);
+        // }
+
+        // //Creation des filiéres
+        // Sector::create(['libele' => 'Banquue Finances Assurances','sigle' => 'BAF']);
+        // Sector::create(['libele' => 'Gestion des resources humaines','sigle' => 'GRH']);
+        // Sector::create(['libele' => 'Genie Logiciel','sigle' => 'GELO']);
+        // Sector::create(['libele' => 'Informatiques et réseaux','sigle' => 'IR']);
+        // Sector::create(['libele' => 'Management des affaires internationales','sigle' => 'MAI']);
+        // Sector::create(['libele' => 'Marketing Management Communication','sigle' => 'MMC']);
+        // Sector::create(['libele' => 'Systéme réseaux et telecom','sigle' => 'SRT']);
 
 
     }
